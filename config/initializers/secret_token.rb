1c4e69b3a87c1f39948c9efcf53d0d9a9e999a4e
daudi amani <dsaronin@gmail.com>
2014-02-19 14:59:19 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleMiliaApp::Application.config.secret_key_base = '033d2bafa6257d0a9092c90301169f9ae73d8a4cb67870967070e7caaf37a3a44182ae8cffde2a2f99d2f55e266ff66d90e8f2c6553ae778a96ff82afe713cde'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SampleMiliaApp::Application.config.session_store :cookie_store, key: '_sample-milia-app_session'
