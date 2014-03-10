1fcd4c031e85d8266a2ca3ddf38e0f43cd7ea7fd
daudi amani <dsaronin@gmail.com>
2014-03-09 23:50:16 -0700

    gemfile changes for heroku deployment - 1

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+SampleMiliaApp::Application.config.secret_token    = '033d2bafa6257d0a9092c90301169f9ae73d8a4cb67870967070e7caaf37a3a44182ae8cffde2a2f99d2f55e266ff66d90e8f2c6553ae778a96ff82afe713cde'
 SampleMiliaApp::Application.config.secret_key_base = '033d2bafa6257d0a9092c90301169f9ae73d8a4cb67870967070e7caaf37a3a44182ae8cffde2a2f99d2f55e266ff66d90e8f2c6553ae778a96ff82afe713cde'
